67025d88c89bf31bd8a8d7a45b3f85eef057e83b
jessan <jessenia.garcia998@gmail.com>
2021-09-15 23:20:53 -0600

    sql and node are connected, log brings up array of candidates from seed.sql

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 //ports
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your sql username
+        user: 'root',
+        //sql password
+        password: 'Twinksquid98',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
 //express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //default response for any other request
 app.use((req, res) => {
